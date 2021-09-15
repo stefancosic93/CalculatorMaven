f89e9e39a0fc5ae1339cc5e13dca910e5dc01629
stefancosic93 <stefan.cosic@cyrillicsoftware.net>
2021-09-15 12:48:08 +0200

    Ver 2

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,5 +1,12 @@
 public class Calculator {
+
+    public static void main (String[] args) {
+        System.out.println("Started the program!");
+        int a = add(1, 5);
+        System.out.println("Result: " + a);
+    }
     public static int add (int num1, int num2) {
+        System.out.println("Called ADD function with: " + num1 + " and " + num2);
         return num1 + num2;
     }
 
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,8 +1,16 @@
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class CalculatorTest {
+
+    @Before
+    public void setUp() {
+        System.out.println("Setup the Maven tests!");
+    }
+
     @Test
     public void add() throws Exception{
         assertEquals(4, Calculator.add(2, 2));
@@ -22,5 +30,9 @@ public class CalculatorTest {
     public void div() {
         assertEquals(1, Calculator.div(2, 2), 0.212121);
     }
-    
+
+    @After
+    public void tearDown() {
+        System.out.println("Tear down the build!");
+    }
 }
